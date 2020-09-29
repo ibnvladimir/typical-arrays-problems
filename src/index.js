a42fa9918a0a8f7d31b75f49a9dc28d6ec32d8f2
ibnvladimir <49831763+ibnvladimir@users.noreply.github.com>
2020-09-29 13:09:59 +0400

    fix: solve no params mistake

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
-exports.min = function min (arr) {
+exports.min = function min (arr = [0]) {
   if (arr.length === 0) return 0;
   return Math.min(...arr);
 }
 
-exports.max = function max (arr) {
+exports.max = function max (arr = [0]) {
   if (arr.length === 0) return 0;
   return Math.max(...arr);
 }
 
-exports.avg = function avg (arr) {
+exports.avg = function avg (arr = [0]) {
   if (arr.length === 0) return 0;
   let sum = arr.reduce((prev, next) => prev + next);
   return sum / arr.length;
