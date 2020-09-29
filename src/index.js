5887607413bf7d59c7bdb61512957f7a48b1956f
ibnvladimir <49831763+ibnvladimir@users.noreply.github.com>
2020-09-29 13:06:26 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (arr.length === 0) return 0;
+  return Math.min(...arr);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (arr.length === 0) return 0;
+  return Math.max(...arr);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (arr.length === 0) return 0;
+  let sum = arr.reduce((prev, next) => prev + next);
+  return sum / arr.length;
 }
